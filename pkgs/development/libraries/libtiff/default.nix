e9d0780556e1e403ff446417befdc6b098eb4d5f
cadkin <adkinscv@ornl.gov>
2024-06-26 17:08:07 -0400

    libtiff: disable lerc if cross-compiling
       - lerc DLLs are not correctly linked into the result directory, so
         disable support for this on Windows targets.
    
    # Conflicts:
    #	pkgs/development/libraries/libtiff/default.nix

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -63,8 +63,9 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ autoreconfHook pkg-config sphinx ];
 
   buildInputs = [
-    lerc
     zstd
+  ] lib.optionals (!stdenv.targetPlatform.isWindows) [
+    lerc
   ];
 
   # TODO: opengl support (bogus configure detection)
